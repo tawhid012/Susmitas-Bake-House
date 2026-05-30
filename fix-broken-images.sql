78e9f2849edb73273e937e9dfd58b899e20f0b21
Tawhid Laskar <tawhid_laskar@Tawhidurs-MacBook-Air.local>
2026-05-30 14:38:44 +0530

    Fix database loading performance and add SQL for broken images

---
+++ fix-broken-images.sql
@@ -0,0 +1,15 @@
+UPDATE products 
+SET image_url = 'sig_floral_1780105847644.png' 
+WHERE image_url LIKE '%1605333550266-9b63a6dc50bc%';
+
+UPDATE products 
+SET image_url = 'sig_truffle_1780105863136.png' 
+WHERE image_url LIKE '%1562214695-1f9e9cf2d5b6%';
+
+UPDATE products 
+SET image_url = 'hero_cake_1780105814884.png' 
+WHERE image_url LIKE '%1623356064972-e1d55f5fbe1a%';
+
+UPDATE products 
+SET image_url = 'about_baking_1780105831919.png' 
+WHERE image_url LIKE '%1571115177098-24edf4c4a45a%';
